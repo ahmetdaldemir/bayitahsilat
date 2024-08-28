e8db7a56b44148d873887ecb14969b3669395f8f
Ahmet DALDEMİR <21971399+ahmetdaldemir@users.noreply.github.com>
2024-08-28 10:10:04 +0300

    Update Database.php

--- sunucu/app/Config/Database.php
+++ sunucu/app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends \CodeIgniter\Database\Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'tah4',
-		'password' => '@198711Ad@',
+		'password' => '@ @',
 		'database' => 'admin_',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
